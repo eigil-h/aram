c7e33bbc10621bcdc1004cbd34152a65c50dd868
eigil-h <eigil.hysver@gmail.com>
2014-03-09 15:42:47 +0100

    inserted gpl3 text to the new system files

--- src/main/cc/service/system.h
+++ src/main/cc/service/system.h
@@ -1,8 +1,19 @@
 /*
- * File:   system.h
- * Author: eigil
- *
- * Created on March 9, 2014, 3:28 PM
+	Warsaw, the audio recorder and music composer
+	Copyright (C) 2014  Eigil Hysvær
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef WARSAW_SYSTEM_H
